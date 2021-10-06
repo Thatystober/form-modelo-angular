901a7be7ee78fda6e0df57dc365b68f87f3b266f
Thatiely Stober Appelt <thatiely.appelt@fortune.local>
2021-10-06 16:29:31 -0300

    Até: Adicione um novo heroi

--- hero-form/src/app/app.module.ts
+++ hero-form/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
@@ -12,7 +13,8 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
